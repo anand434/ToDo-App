c16c5541ac0c562de75bf0379c07adb9c2cc4488
anand434 <kr.anand434@gmail.com>
2017-08-27 17:17:55 +0530

    some changes

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.task, name='task'),
+    # /(?P<id>\w{0,50})/ this is the id of the current cursor
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    url(r'^add', views.add, name='add'),
+
 ]
 
 
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+
 
 def task(request):
     todos = Todo.objects.all()[:]
@@ -9,3 +10,25 @@ def task(request):
         'todos': todos
     }
     return render(request, 'index.html', context)
+
+
+def details(request, id):
+    todo = Todo.object.get(id=id)
+    context = {
+        'todo': todo
+    }
+    return render(request, 'details.html', context)
+
+
+def add(request):
+    if request.method == 'POST':
+        # get the data
+        title = request.POST['title']
+        text = request.POST['text']
+
+        # push into sqlite3
+        todo = Todo(title=title, text=text)
+        todo.save()
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
